cdc1d1fd9ef07a6066a82f1d09b6cde2b1040c87
shobharathor <shobharathor1990@gmail.com>
2024-01-19 16:31:40 +0530

    make stopwatch

---
+++ script.js
@@ -0,0 +1,47 @@
+let[milliseconds, second,minutes,hours] = [0,0,0,0];
+let timeRef = document.querySelector(".timer-display");
+let int = null;
+
+document.getElementById("start-timer").addEventListener("click",()=>{
+    if(int != null){
+        clearInterval(int);
+    }
+    int = setInterval(displayTimer,10);
+});
+
+document.getElementById("pause-timer").addEventListener("click",()=>{
+
+     clearInterval(int);
+
+});
+
+document.getElementById("reset-timer").addEventListener("click",()=>{
+    clearInterval(int);
+    [milliseconds, second,minutes,hours] = [0,0,0,0];
+    timeRef.innerHTML = "00 : 00 : 00 : 000";
+});
+
+function displayTimer(){
+    milliseconds += 10;
+    if(milliseconds == 1000){
+        milliseconds = 0;
+        second++;
+        if(second == 60){
+            second = 0;
+            minutes++;
+            if(milliseconds == 60){
+                minutes = 0;
+                hours++;
+            }
+        }
+    }
+
+    let h = hours < 10 ? "0" + hours : hours;
+    let m = minutes < 10 ? "0" + minutes : minutes;
+    let s = second < 10 ? "0" + second : second;
+
+    let ms = milliseconds < 10 ? "00" + milliseconds : milliseconds;
+
+
+    timeRef.innerHTML = ` ${h} : ${m} : ${s} : ${ms} `;
+}
